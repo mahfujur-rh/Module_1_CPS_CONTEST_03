035f2f3b64e413c432c85f2a45ae2c6ed541037e
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 20:57:51 +0600

    Create problem-06.cpp

---
+++ problem-06.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/SUNNYDAY
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc;
+   cin>>tc;
+
+    while(tc--){
+        int n;
+        cin>>n;
+        if(n>24){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+
+    }
+    return 0;
+}
