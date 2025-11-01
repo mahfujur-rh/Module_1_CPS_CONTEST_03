428ffef1d7767e2dcd5636f28aecab2348c38406
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 21:00:31 +0600

    Create problem-07.cpp

---
+++ problem-07.cpp
@@ -0,0 +1,17 @@
+//https://atcoder.jp/contests/abc336/tasks/abc336_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "L";
+    for (int i = 1; i <= n; i++) {
+        cout << "o";
+    }
+    cout << "ng\n";
+
+    return 0;
+}
