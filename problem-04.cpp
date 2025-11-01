28b5db367b66205b7946af149eead29b35cf2a80
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 20:53:24 +0600

    Create problem-04.cpp

---
+++ problem-04.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/DNATION
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;cin>>tc;
+    while(tc--){
+        int x,y;cin>>x>>y;
+        int total =y-x;
+        cout<<total<<endl;
+    }
+    return 0;
+
+}
