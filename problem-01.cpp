f781c4c1778b38d5937019f3f9c1373af187865c
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 20:49:55 +0600

    Create problem-01.cpp

---
+++ problem-01.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/AGEING
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int tc;cin>>tc;
+    while(tc--){
+        int x;cin>>x;
+        int total=x-10;
+        cout<<total<<endl;
+    }
+    return 0;
+
+}
+
