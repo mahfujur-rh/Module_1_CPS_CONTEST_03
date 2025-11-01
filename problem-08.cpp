732d771b497974b5e6c181927c8e00393112b179
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-11-01 21:04:03 +0600

    Create problem-08.cpp

---
+++ problem-08.cpp
@@ -0,0 +1,23 @@
+//https://atcoder.jp/contests/abc328/tasks/abc328_a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int s;
+        cin >> s;
+        if (s <= x) {
+            sum += s;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
